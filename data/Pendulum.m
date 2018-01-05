3698293c4d358113ab0827b7b2b99a6f769e9dde
Bethany Lusch <herwaldt@uw.edu>
2018-01-05 14:17:17 -0800

    add files to generate Pendulum dataset

---
+++ data/Pendulum.m
@@ -0,0 +1,26 @@
+
+numICs = 200*200*21/20;
+filenamePrefix = 'Pendulum';
+
+x1range = [-3.1,3.1];
+x2range = [-2, 2];
+tSpan = 0:0.02:1;
+
+max_potential = .99;
+
+seed = 1;
+X_test = PendulumFnPotential(x1range, x2range, round(.1*numICs), tSpan, seed, max_potential);
+filename_test = strcat(filenamePrefix, '_test_x.csv');
+dlmwrite(filename_test, X_test, 'precision', '%.14f')
+
+seed = 2;
+X_val = PendulumFnPotential(x1range, x2range, round(.2*numICs), tSpan, seed, max_potential);
+filename_val = strcat(filenamePrefix, '_val_x.csv');
+dlmwrite(filename_val, X_val, 'precision', '%.14f')
+
+for j = 1:10
+	seed = 2+j;
+	X_train = PendulumFnPotential(x1range, x2range, round(.7*numICs), tSpan, seed, max_potential);
+	filename_train = strcat(filenamePrefix, sprintf('_train%d_x.csv', j));
+	dlmwrite(filename_train, X_train, 'precision', '%.14f')
+end
---
+++ data/PendulumFnPotential.m
@@ -0,0 +1,41 @@
+function X = PendulumFnPotential(x1range, x2range, numICs, tSpan, seed, max_potential)
+
+
+% try some initial conditions for x1, x2
+rng(seed)
+
+dynsys = @(t,x) [x(2,:); -sin(x(1,:))];
+
+lenT = length(tSpan);
+
+X = zeros(numICs*lenT, 2);
+
+potential = @(x,y) (1/2)*y^2-cos(x);
+
+count = 1;
+for j = 1:100*numICs
+    % randomly start from x1range(1) to x1range(2)
+    x1 = (x1range(2)-x1range(1))*rand+x1range(1);
+
+    % randomly start from x2range(1) to x2range(2)
+    x2 = (x2range(2)-x2range(1))*rand+x2range(1);
+
+
+    if potential(x1, x2) <= max_potential
+        ic = [x1; x2];
+
+        [T, temp] = ode45(dynsys, tSpan, ic);
+
+        X(1+(count-1)*lenT : lenT + (count-1)*lenT,:) = temp;
+	if count == numICs
+	    break
+	end
+        count = count + 1;
+    end
+end
+
+if count < numICs
+	sprintf('oops, potential energy too small for IC box')
+end
+
+
