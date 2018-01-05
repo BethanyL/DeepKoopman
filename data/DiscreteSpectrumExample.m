814acae4a0b157590f0d8cfa6adcfbcecd5ba292
Bethany Lusch <herwaldt@uw.edu>
2018-01-05 14:13:43 -0800

    initial commit, fns to generate DiscreteSpectrumExample datasets

---
+++ data/DiscreteSpectrumExample.m
@@ -0,0 +1,28 @@
+
+numICs = 200*200*21/20;
+filenamePrefix = 'DiscreteSpectrumExample';
+
+x1range = [-.5, .5];
+x2range = x1range;
+tSpan = 0:0.02:1;
+mu = -0.05;
+lambda = -1;
+
+seed = 1;
+X_test = DiscreteSpectrumExampleFn(x1range, x2range, round(.1*numICs), tSpan, mu, lambda, seed);
+filename_test = strcat(filenamePrefix, '_test_x.csv');
+dlmwrite(filename_test, X_test, 'precision', '%.14f')
+
+seed = 2;
+X_val = DiscreteSpectrumExampleFn(x1range, x2range, round(.2*numICs), tSpan, mu, lambda, seed);
+filename_val = strcat(filenamePrefix, '_val_x.csv');
+dlmwrite(filename_val, X_val, 'precision', '%.14f')
+
+for j = 1:10
+	seed = 2+j;
+	X_train = DiscreteSpectrumExampleFn(x1range, x2range, round(.7*numICs), tSpan, mu, lambda, seed);
+	filename_train = strcat(filenamePrefix, sprintf('_train%d_x.csv',j));
+	dlmwrite(filename_train, X_train, 'precision', '%.14f')
+end
+
+
---
+++ data/DiscreteSpectrumExampleFn.m
@@ -0,0 +1,38 @@
+function X = DiscreteSpectrumExampleFn(x1range, x2range, numICs, tSpan, mu, lambda, seed)
+
+% Koopman example from 3.4.2 (pg 51) of the DMD book
+% nonlinear dynamical system in two variables, but with 3D Koopman
+% observables, have linear dynamical system
+
+% try some initial conditions for x1, x2
+rng(seed)
+
+% randomly start from x1range(1) to x1range(2)
+x1 = (x1range(2)-x1range(1))*rand([numICs,1])+x1range(1);
+
+% randomly start from x2range(1) to x2range(2)
+x2 = (x2range(2)-x2range(1))*rand([numICs,1])+x2range(1);
+
+lenT = length(tSpan);
+
+X = zeros(numICs*lenT, 2);
+
+count = 1;
+for j = 1:numICs
+    Y0 = [x1(j); x2(j); x1(j)^2];
+    c1 = Y0(1);
+    c2 = Y0(2) + (lambda*Y0(3))/(2*mu-lambda);
+    c3 = (-lambda*Y0(3))/(2*mu-lambda);
+    c4 = Y0(3);
+    Y = [c1 * exp(mu*tSpan);
+        c2 * exp(lambda*tSpan) + c3 * exp(2*mu*tSpan);
+        c4 * exp(2*mu*tSpan)];
+
+    Xhat = Y(1:2,:);
+    X(1+(count-1)*lenT : lenT + (count-1)*lenT,:) = Xhat(:,1:lenT)';
+    count = count + 1;
+end
+
+
+
+
